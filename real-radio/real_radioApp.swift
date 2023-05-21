d1b393fde0fdff0477c00736bb7795152e586b62
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-05-21 13:40:35 +0100

    Initial Commit

---
+++ real-radio/real_radioApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_radioApp.swift
+//  real-radio
+//
+//  Created by Josh Ellis on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct real_radioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
